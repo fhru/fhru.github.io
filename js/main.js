4d3c4cc1613b21caf32d60ae75a5327b6b8f4966
fhru <fhrurhmn@gmail.com>
2022-12-06 14:37:08 +0700

    Updated

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var btnContainer = document.getElementById("interest");
+
+var btns = btnContainer.getElementsByClassName("interest-btn");
+
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
