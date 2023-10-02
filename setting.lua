a3322c94b52d08b0bda173f2881bdd764be237c6
fhru <fhrurhmn@gmail.com>
2023-10-02 14:33:30 +0700

    update

---
+++ setting.lua
@@ -0,0 +1,9 @@
+world_manager = getWorldManager()
+
+world_manager:addFarm("FZXIC:PUBLIKAJG1", 5666)
+world_manager:addFarm("VHAHN:PUBLIKAJG1", 5666)
+world_manager:addFarm("GGJYG:PUBLIKAJG1", 5666)
+world_manager:addFarm("WHYEI:PUBLIKAJG1", 5666)
+world_manager:addFarm("SLZPB:PUBLIKAJG1", 5666)
+world_manager:addStorage("GSOLJELEK:SEEDBAL23", StorageType.seed, 5667)
+world_manager:addStorage("GSOLJELEK:MEKIM431", StorageType.pack, 0)
